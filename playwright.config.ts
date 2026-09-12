8512fb6cee79a6ee6274e81ad183cf5781830576
alivabasa <aliva123basa@gmail.com>
2026-09-13 01:33:13 +0530

    In this commit, I have made modifications to the following files: 1. .github/workflows/playwright.yml: Updated the Playwright workflow configuration. 2. metrics/metrics.json: Made changes to the metrics data. 3. playwright.config.ts: Updated the Playwright configuration settings.

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,10 +22,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
+  reporter: process.env.CI ?
+  [
+    ['blob'],
+    ['./reporters/MetricsReporter.ts'],
+  ]
+  :
+  [
     ['html'],
     ['json', { outputFile: 'playwright-report/results.json' }],
     ['./reporters/MetricsReporter.ts'],
+
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
